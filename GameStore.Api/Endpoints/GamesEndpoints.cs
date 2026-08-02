ca051820ee6741ee3a04492b874a9d0f0c43d8f5
Rudy Liljeberg <rudy@winnebagowebdesigns.com>
2026-08-02 00:00:38 -0500

    Added async support to API endpoints

--- GameStore.Api/Endpoints/GamesEndpoints.cs
+++ GameStore.Api/Endpoints/GamesEndpoints.cs
@@ -7,52 +7,51 @@ namespace GameStore.Api.Endpoints;
 using GameStore.Api.Data;
 using GameStore.Api.Dtos;
 using GameStore.Api.Models;
+using Microsoft.EntityFrameworkCore;
 
 public static class GamesEndpoints
 {
 
     const string GetGameEndpointName = "GetGame"; // Creates constant for identifying games by their name
 
-    private static readonly List<GameDto> games = [
-        new (
-            1,
-            "Street Fighter II",
-            "Fighting",
-            19.99M,
-            new DateOnly(1992, 7, 15)),
-        new (
-            2,
-            "Final Fantasy VII Rebirth",
-            "RPG",
-            69.99M,
-            new DateOnly(2024, 2, 29)),
-        new (
-            3,
-            "Astro Bot",
-            "Platformer",
-            59.99M,
-            new DateOnly(2024, 9, 6))
-    ]; // Creates a static list of GameDto objects to represent the games in the database
-
     /** Configures HTTP request pipeline (Defines what happens when HTTP requests start arriving into application) **/
     public static void MapGamesEndpoint(this WebApplication app)
     {
         var group = app.MapGroup("/games"); // Creates a group of endpoints for handling requests related to games, with the base URL "/games"
 
-        // GET /games
-        group.MapGet("/", () => games); // Maps "GET .../games" to display all games in the database
+        // GET /games, Maps "GET .../games" to asynchronously display all games currently in the database
+        group.MapGet("/", async (GameStoreContext dbContext) 
+            => await dbContext.Games
+                              .Include(game => game.Genre) // Ensures that the game's Genre will be included in this selection                            
+                              .Select(game => new GameSummaryDto(
+                                game.Id,
+                                game.Name,
+                                game.Genre!.Name,
+                                game.Price,
+                                game.ReleaseDate
+                              )) // Projects each game in our Games table in the database as a new GameSummaryDto object
+                              .AsNoTracking() // Tells EF Core not to track any changes from the selection process
+                              .ToListAsync()); // Asynchronously creates a list of type GameSummaryDto out of the projected game objects
 
-        // GET /games/1, Maps "GET .../games/{id}" to display a game in the database by its id #
-        group.MapGet("/{id}", (int id) =>
+        // GET /games/1, Maps "GET .../games/{id}" to asynchronously display a game in the database by its id #
+        group.MapGet("/{id}", async (int id, GameStoreContext dbContext) =>
         {
-            var game = games.Find(game => game.Id == id); // Finds the game with the specified id in the database
+            var game = await dbContext.Games.FindAsync(id); // Asynchronously finds the game with the specified id in the database
 
-            return game is null ? Results.NotFound() : Results.Ok(game); // Returns a 404 Not Found response if the game with the specified id is not found in the database, otherwise returns a 200 OK response with details of the specified game
+            return game is null ? Results.NotFound() : Results.Ok(
+                new GameDetailsDto(
+                    game.Id,
+                    game.Name,
+                    game.GenreId,
+                    game.Price,
+                    game.ReleaseDate
+                ) // Returns a 404 Not Found response if the game with the specified id is not found in the database, otherwise returns a 200 OK response with details of the specified game as a new GameDetailsDto object
+            );
         })
             .WithName(GetGameEndpointName); // Assigns unique identifier to this request for use in other requests
 
         // POST /games
-        group.MapPost("/", (CreateGameDto newGame, GameStoreContext dbContext) =>
+        group.MapPost("/", async (CreateGameDto newGame, GameStoreContext dbContext) => // NOTE: Always include "async" and "await" keywords in endpoints to denote the use of asynchronous methods during execution
         {
             Game game = new()
             {
@@ -60,10 +59,10 @@ public static class GamesEndpoints
                 GenreId = newGame.GenreId,
                 Price = newGame.Price,
                 ReleaseDate = newGame.ReleaseDate
-            }; // Defines game model to insert into the database
+            }; // Defines game model as a means of providing information for new game entry into database
 
             dbContext.Games.Add(game); // Asks EntityFrameworkCore to begin tracking that a new game needs to be inserted into the database
-            dbContext.SaveChanges(); // Translates any pending changes in the DbContext change tracker into SQL statements that the database can understand
+            await dbContext.SaveChangesAsync(); // Asynchronously saves all changes made in this context to the database by translating any pending changes in the DbContext change tracker into SQL statements that the database can understand, before then executing said statements
 
             GameDetailsDto gameDto = new(
                 game.Id,
@@ -71,40 +70,46 @@ public static class GamesEndpoints
                 game.GenreId,
                 game.Price,
                 game.ReleaseDate
-            );
+            ); // Creates a new object of type GameDetailsDto using information provided 
 
             return Results.CreatedAtRoute(GetGameEndpointName, new { id = gameDto.Id }, gameDto); // Returns a 201 Created response containing the details of the added game, and includes a Location header with the URI of the newly created game resource
-        }); // Maps "POST .../games" to add a game to the database as an instance of the model Game object, 
+        }); // Maps "POST .../games" to asynchronously add a game to the database as an instance of the model Game object, 
             // expects CreateGameDto object to define the new game model to insert into the database,
             // as well as an instance of GameStoreContext in order to store game into the database using said context
 
         // PUT /games/1
-        group.MapPut("/{id}", (int id, UpdateGameDto updatedGame) =>
+        group.MapPut("/{id}", async (
+            int id, 
+            UpdateGameDto updatedGame, 
+            GameStoreContext dbContext) =>
         {
-            var index = games.FindIndex(game => game.Id == id); // Finds the index of the game with the specified id in the database
+            var existingGame = await dbContext.Games.FindAsync(id); // Asynchronously finds the game with the specified id in the database
 
-            if (index == -1)
+            if (existingGame is null)
             {
-                return Results.NotFound(); // Returns a 404 Not Found response if the game with the specified id is not found in the database
+                return Results.NotFound(); // Returns a 404 Not Found response if the specified game does not yet exist in the database
             }
 
-            games[index] = new GameDto(
-                id,
-                updatedGame.Name,
-                updatedGame.Genre,
-                updatedGame.Price,
-                updatedGame.ReleaseDate
-            ); // Updates the game with the specified id in the database with the new values from updatedGame. Not threat-safe.
+            existingGame.Name = updatedGame.Name; // Updates the name of the existing game at the specified id in the database to that of updatedGame
+            existingGame.GenreId = updatedGame.GenreId; // Updates the genre id of the existing game at the specified id in the database to that of updatedGame
+            existingGame.Price = updatedGame.Price; // Updates the price of the existing game at the specified id in the database to that of updatedGame
+            existingGame.ReleaseDate = updatedGame.ReleaseDate; // Updates the release date of the existing game at the specified id in the database to that of updatedGame
+
+            await dbContext.SaveChangesAsync(); // Asynchronously saves all changes made in this context to the database by translating any pending changes in the DbContext change tracker into SQL statements that the database can understand, before then executing said statements
 
             return Results.NoContent(); // Returns a 204 No Content response to indicate that the update was successful, but that there is no content to return in the response body
-        }); // Maps "PUT .../games/{id}" to update a game in the database by its id #, expects int id to match "/games/{id}" and UpdateGameDto object to update the game with
+        }); // Maps "PUT .../games/{id}" to asynchronously update a game in the database by its id #, expects int id to match "/games/{id}" and UpdateGameDto object to update the game with
 
         // DELETE /games/1
-        group.MapDelete("/{id}", (int id) =>
+        group.MapDelete("/{id}", async (int id, GameStoreContext dbContext) =>
         {
-            games.RemoveAll(game => game.Id == id); // Removes the game with the specified id from the database
+           await dbContext.Games
+                          .Where(game => game.Id == id) // Filters all games in the database by the id provided in the method header.
+                          .ExecuteDeleteAsync(); // Asynchronously deletes any games that match the id provided in the method header. 
+                                                 // NOTE: No SaveChangesAsync() is needed as ExecuteDeleteAsync() will delete the game from the database on its own (assuming the game already existed in the database to begin with).
+                        
 
             return Results.NoContent(); // Returns a 204 No Content response whether or not the game with the specified id was found and removed from the database
-        }); // Maps "DELETE .../games/{id}" to delete a game in the database by its id # regardless of whether or not there is a game with an id # in the database that matches the one provided, expects int id to match "/games/{id}"
+        }); // Maps "DELETE .../games/{id}" to asynchronously delete a game in the database by its id # regardless of whether or not there is a game with an id # in the database that matches the one provided, expects int id to match "/games/{id}"
     }
 }
--- GameStore.Api/obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a40a0839091c7c93cce0a55ac588e62cd7474c2c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7de9f1a52396dbcc45cdce82cbae52352a99cb0")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
